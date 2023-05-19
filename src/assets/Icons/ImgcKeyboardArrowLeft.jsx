82efa3d46034d2541d1a3767d84520a1ec27ca1f
IvanMGC <igarciacaliirgos@gmail.com>
2023-05-19 15:20:56 -0500

    Slider modificado

---
+++ src/assets/Icons/ImgcKeyboardArrowLeft.jsx
@@ -0,0 +1,15 @@
+export default function ImgcKeyboardArrowLeft(props){
+    return(
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={48}
+            height={48}
+            viewBox="0 96 960 960"
+            {...props}
+        >
+            <path d="M453 815 213 575l240-240 42 42-198 198 198 198-42 42Zm253 0L466 575l240-240 42 42-198 198 198 198-42 42Z" 
+            fill="white"
+            />
+        </svg>
+    )
+}
